19c69ae4d728dffc5b7884abdc3606fd7c9d7f2f
Aidan Thiele <a.engelthiele@gmail.com>
2021-11-14 18:50:16 -0800

    Gets rounds

---
+++ simoneGame.js
@@ -0,0 +1,11 @@
+let numRounds = 0;
+let startButton = document.getElementById("play");
+startButton.addEventListener("click", runGame());
+
+function updateRounds(){
+    numRounds = document.getElementById("rounds").innerHTML;
+}
+
+function runGame(){
+    //Run game here
+}
