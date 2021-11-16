0c4122f4ddea15b59b21f3b26ce98f305ad4afe3
Aidan Thiele <a.engelthiele@gmail.com>
2021-11-15 23:36:02 -0800

    added basic win/loss functionality, working on completion

--- simoneGame.js
+++ simoneGame.js
@@ -1,12 +1,7 @@
 let numRounds = 0;
 let sequence = [];
 let clickedSequence = [];
-
-//Sounds
-let redSound = new Audio('sounds/red.wav');
-let blueSound = new Audio('sounds/blue.wav');
-let greenSound = new Audio('sounds/green.wav');
-let yellowSound = new Audio('sounds/yellow.wav');
+let lookingForInput = false;
 
 //Light sections
 let startButton = document.getElementById("play");
@@ -15,18 +10,27 @@ let redLight = document.getElementById("redSq");
 let blueLight = document.getElementById("blueSq");
 let yellowLight = document.getElementById("yellowSq");
 
-startButton.addEventListener("click", runGame);
+startButton.addEventListener("click", getSolution);
+
 greenLight.addEventListener("click", () => {
-    clickedSequence[clickedSequence.length] = 4;
+    clickedSequence[clickedSequence.length] = 1;
+    getUserInput();
+    console.log(clickedSequence);
 });
 redLight.addEventListener("click", () => {
     clickedSequence[clickedSequence.length] = 2;
+    getUserInput();
+    console.log(clickedSequence);
 });
 blueLight.addEventListener("click", () => {
     clickedSequence[clickedSequence.length] = 3;
+    getUserInput();
+    console.log(clickedSequence);
 });
 yellowLight.addEventListener("click", () => {
     clickedSequence[clickedSequence.length] = 4;
+    getUserInput();
+    console.log(clickedSequence);
 });
 
 //Updates the number of rounds to play
@@ -36,25 +40,23 @@ function updateRounds(){
 }
 
 //Runs the game
-function runGame(){
+function getSolution(){
     updateRounds();
     for(let round = 0; round < numRounds; round++){
         sequence[round] = Math.ceil(Math.random() * 4);
     }
-    console.log(sequence);
     let greeting = new Promise((resolve, reject) => {
-        resolve(greet());
+        resolve(start());
         reject("Error, greeting failed");
-    })
+    });
     greeting.then(() => {
-
-        for(let round = 0; round < numRounds; round++){
-            //console.log(sequence[round]);
-
+        runRounds();
+        async function runRounds(){
+            let round = 0;
             let sequenceRun = new Promise((resolve, reject) => {
                 resolve(playSequence());
                 reject("Error, displaying sequence failed");
-            })
+            });
             sequenceRun.then(() => {
                 clickedSequence = [];
                 let waitForInput = new Promise((resolve, reject) => {
@@ -62,29 +64,23 @@ function runGame(){
                         resolve();
                     }
                 })
-            })
+            });
             //Plays the sequence of colors
             async function playSequence(){
                 let wait = await waitAMoment(4);
-                //console.log("sequencing");
                 for(let sequenceNum = 0; sequenceNum <= round; sequenceNum++){
-                    //console.log(sequence);
                     if(sequence[sequenceNum] == 1){
-                        //console.log("green");
                         greenLight.className = "lightgreen";
-                        greenSound.play();
+                        (new Audio("sounds/green.wav")).play();
                     } else if (sequence[sequenceNum] == 2){
-                        //console.log("red");
                         redLight.className = "lightred";
-                        redSound.play();
+                        (new Audio("sounds/red.wav")).play();
                     } else if(sequence[sequenceNum] == 3){
-                        //console.log("blue");
                         blueLight.className = "lightblue";
-                        blueSound.play();
+                        (new Audio("sounds/blue.wav")).play();
                     } else {
-                        //console.log("yellow");
                         yellowLight.className = "lightyellow";
-                        yellowSound.play();
+                        (new Audio("sounds/yellow.wav")).play();
                     }
                 
                     wait = await waitAMoment(.15);
@@ -100,37 +96,68 @@ function runGame(){
                         yellowLight.className = "yellow";
                     }
 
-                    wait = await waitAMoment(1);
+                    wait = await waitAMoment(.25);
+                }
+
+                lookingForInput = true;
+                clickedSequence = [];
+                console.log("getting input");
+
+                if(round < numRounds ){
+                    round++;
+                    await getUserInput(round);
+                    playSequence();
+                } else {
+                    console.log("done")
                 }
             }
         }
-    })
+    });
+}
+
+function getUserInput(round = 0){
+    while(lookingForInput){
+        console.log("inputting");
+        return new Promise((resolve, reject) => {
+            if(clickedSequence[clickedSequence.length - 1] != sequence[clickedSequence.length - 1]){
+                console.log("Loss Resolution");
+                lookingForInput = false;
+                lose();
+                resolve();
+            }
+            if(clickedSequence.length >= round){
+                console.log("correct resolution");
+                lookingForInput = false;
+                resolve();
+            }
+        });
+    }
 }
 
 //Plays a random sequence of 12 blinking lights to greet the user before playing
-async function greet(){
+async function start(){
     for(let num = 0; num < 12; num++){
 
         //Selects one of the random light colors
         let lightColor = Math.ceil(Math.random() * 4);
-        let wait = await waitAMoment(.25);
+        let wait = await waitAMoment(.06);
 
         //Brightens selected color
         if(lightColor == 1){
             greenLight.className = "lightgreen";
-            greenSound.play();
+            (new Audio("sounds/green.wav")).play();
         } else if (lightColor == 2){
             redLight.className = "lightred";
-            redSound.play();
+            (new Audio("sounds/red.wav")).play();
         } else if(lightColor == 3){
             blueLight.className = "lightblue";
-            blueSound.play();
+            (new Audio("sounds/blue.wav")).play();
         } else {
             yellowLight.className = "lightyellow";
-            yellowSound.play();
+            (new Audio("sounds/yellow.wav")).play();
         }
 
-        wait = await waitAMoment(.15);
+        wait = await waitAMoment(.06);
 
         //Returns colors to original values
         if(lightColor == 1){
@@ -142,6 +169,8 @@ async function greet(){
         } else {
             yellowLight.className = "yellow";
         }
+
+        wait = await waitAMoment(.12)
     }
 }
 
@@ -156,15 +185,15 @@ function waitAMoment(time = 1.0){
 
 function nextRound(){
     let nextRoundSound = 'sounds/nextRound.wav';
-    nextRoundSound.play();
+    (new Audio(nextRoundSound)).play();
 }
 
 function lose(){
     let loseSound = 'sounds/lose.wav';
-    loseSound.play();
+    (new Audio(loseSound)).play();
 }
 
 function win(){
     let winSound = 'sounds/win.mp3';
-    winSound.play();
+    (new Audio(winSound)).play();
 }
